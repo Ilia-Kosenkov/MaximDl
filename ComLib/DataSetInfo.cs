3a2c354adf61aaec5053921a4f886285934d9dae
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2019-11-25 14:01:56 +0200

    Paring paths

---
+++ ComLib/DataSetInfo.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO.Abstractions;
+using System.Collections.Immutable;
+using System.Linq;
+internal class DataSetInfo
+{
+    public static DataSetInfo Empty { get; } = new DataSetInfo(Enumerable.Empty<IFileSystemInfo>());
+    public ImmutableList<IFileSystemInfo> Files {get;}
+    public ImmutableList<IFileSystemInfo> Bias {get;}
+    public ImmutableList<IFileSystemInfo> Dark {get;}
+
+    public DataSetInfo(
+        IEnumerable<IFileSystemInfo> files, 
+        IEnumerable<IFileSystemInfo> bias,
+        IEnumerable<IFileSystemInfo> dark)
+    {
+        Files = files?.ToImmutableList() ?? ImmutableList<IFileSystemInfo>.Empty;
+        Bias = bias?.ToImmutableList()  ?? ImmutableList<IFileSystemInfo>.Empty;
+        Bias = dark?.ToImmutableList()  ?? ImmutableList<IFileSystemInfo>.Empty;
+    }
+
+    public DataSetInfo(
+        IEnumerable<IFileSystemInfo> allFiles,
+        string biasPattern = "_bias_",
+        string darkPattern = "_dark_")
+    {
+        var biasRegex = new Regex(biasPattern, RegexOptions.Compiled);
+        var darkRegex = new Regex(darkPattern, RegexOptions.Compiled);
+
+        var filesBuilder = ImmutableList.CreateBuilder<IFileSystemInfo>();
+        var biasBuilder = ImmutableList.CreateBuilder<IFileSystemInfo>();
+        var darkBuilder = ImmutableList.CreateBuilder<IFileSystemInfo>();
+
+        foreach(var file in allFiles)
+        {
+            if(biasRegex.IsMatch(file.Name))
+                biasBuilder.Add(file);
+            else if (darkRegex.IsMatch(file.Name))
+                darkBuilder.Add(file);
+            else
+                filesBuilder.Add(file);
+        }
+
+        Files = filesBuilder.ToImmutable();
+        Bias = biasBuilder.ToImmutable();
+        Dark = darkBuilder.ToImmutable();
+    }
+}
--- ComLib/Program.cs
+++ ComLib/Program.cs
@@ -1,17 +1,32 @@
 ﻿using System;
-using System.Threading.Tasks;
+using System.IO;
+using System.Linq;
+using Ganss.IO;
 
 namespace MaximDl
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
+        {
+            ParseArgs(args);
+        }
+
+        private static DataSetInfo ParseArgs(string[] args)
+            => args switch
+            {
+                _ when args.Length == 1 => new DataSetInfo(Glob.Expand(args[0])),
+                _ when args.Length == 3 => new DataSetInfo(Glob.Expand(args[0]), Glob.Expand(args[1]), Glob.Expand(args[2])),
+                _ => DataSetInfo.Empty
+            };
+        private static void Calibrate()
         {
             using var app = MaxImDlApp.Acquire();
             using var doc = MaxImDlDoc.Acquire();
             app.CalClear();
             app.CalMedianBias = true;
             app.CalMedianDark = true;
+
             app.CalAddBias(@"C:\NOT_July2019\Correct\Data\Night_1\MAXIJ1820-POL_bias_B_0001.fits");
             app.CalAddDark(@"C:\NOT_July2019\Correct\Data\Night_1\MAXIJ1820-POL_dark_B_0001.fits");
             app.CalSet();
@@ -19,15 +34,6 @@ namespace MaximDl
             doc.OpenFile(@"C:\NOT_July2019\Correct\Data\Night_1\MAXIJ1820-POL_B_0001.fits");
             doc.Calibrate();
             doc.Bin(2);
-
-            while(doc.MouseNewClick == 0)
-            {
-                await Task.Delay(TimeSpan.FromMilliseconds(250));
-            }
-
-            Console.WriteLine($"({doc.MouseX}, {doc.MouseY})");
-            var result = doc.CalcInformation(doc.MouseX, doc.MouseY, new Ring(10, 2, 10));
-
         }
 
       
