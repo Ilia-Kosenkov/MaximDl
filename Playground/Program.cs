fe1afa224fa63710134706a4d6b8d071d3321222
Ilia <ilia.kosenkov.at.gm@gmail.com>
2019-11-27 09:33:46 +0200

    Testing grounds project

---
+++ Playground/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MaximDl;
+
+namespace Playground
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using var app = MaxImDlApp.Acquire();
+
+            using var doc = app.CreateDocument();
+            doc.OpenFile("");
+            Console.ReadKey();
+            doc.Close();
+        }
+    }
+}
